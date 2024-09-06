075b1a14c18d3516a80d6bb26f3e70028cbcc520
Dharya Pratap Singh <73015898+dharya07@users.noreply.github.com>
2024-09-06 06:28:50 +0000

    Added input validation to handle non-integer inputs and improved the structure for better readability.

--- main.py
+++ main.py
@@ -29,31 +29,30 @@ scissors = '''
 
 game_images = [rock, paper, scissors]
 
-user_choice = int(
-    input("What do you choose? Type 0 for Rock, 1 for Paper, 2 for Scissors.\n"))
+def get_user_choice():
+    while True:
+        try:
+            choice = int(input("What do you choose? Type 0 for Rock, 1 for Paper, 2 for Scissors.\n"))
+            if choice in [0, 1, 2]:
+                return choice
+            else:
+                print("Invalid choice. Please enter 0, 1, or 2.")
+        except ValueError:
+            print("Invalid input. Please enter a number.")
 
+user_choice = get_user_choice()
 computer_choice = random.randint(0, 2)
 
-if user_choice >= 0 and user_choice <= 2:
-    print(game_images[user_choice])
-    print("Computer chose:")
-    print(game_images[computer_choice])
-    if user_choice == computer_choice:
-        print("Draw")
-    elif user_choice == 0:
-        if computer_choice == 1:
-            print("You lose")
-        else:
-            print("You win!")
-    elif user_choice == 1:
-        if computer_choice == 2:
-            print("You lose")
-        else:
-            print("You win!")
-    elif user_choice == 2:
-        if computer_choice == 0:
-            print("You lose")
-        else:
-            print("You win!")
+print("You chose:")
+print(game_images[user_choice])
+print("Computer chose:")
+print(game_images[computer_choice])
+
+if user_choice == computer_choice:
+    print("It's a draw!")
+elif (user_choice == 0 and computer_choice == 2) or \
+     (user_choice == 1 and computer_choice == 0) or \
+     (user_choice == 2 and computer_choice == 1):
+    print("You win!")
 else:
-    print("You enetered an invalid choice! You lose!")
+    print("You lose!")
